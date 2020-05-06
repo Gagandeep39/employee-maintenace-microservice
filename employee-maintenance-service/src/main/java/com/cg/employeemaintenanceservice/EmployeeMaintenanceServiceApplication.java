aedd935c7ae79e3ea2f520a3241d86b07deb601b
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-05-07 00:58:30 +0530

    Created Employee Maintenance service Created All entity classes Added prod, dev profile Added Dummy data

---
+++ employee-maintenance-service/src/main/java/com/cg/employeemaintenanceservice/EmployeeMaintenanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cg.employeemaintenanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMaintenanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeMaintenanceServiceApplication.class, args);
+    }
+
+}
