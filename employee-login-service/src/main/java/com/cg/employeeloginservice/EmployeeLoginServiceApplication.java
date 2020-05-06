bc1fef3b0d0597f6de38712cf2c65cbadda90004
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-05-06 18:31:49 +0530

    Initial Commit Created Dev and Prod profile for different databases

---
+++ employee-login-service/src/main/java/com/cg/employeeloginservice/EmployeeLoginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cg.employeeloginservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeLoginServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeLoginServiceApplication.class, args);
+    }
+
+}
