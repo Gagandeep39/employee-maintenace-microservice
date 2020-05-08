874ec3dee72af2cced044c5855a19976adeefb50
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-05-08 07:45:04 +0530

    Added Hystrix Support

--- employee-maintenance-service/src/main/java/com/cg/employeemaintenanceservice/EmployeeMaintenanceServiceApplication.java
+++ employee-maintenance-service/src/main/java/com/cg/employeemaintenanceservice/EmployeeMaintenanceServiceApplication.java
@@ -2,8 +2,12 @@ package com.cg.employeemaintenanceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrix
 public class EmployeeMaintenanceServiceApplication {
 
     public static void main(String[] args) {
