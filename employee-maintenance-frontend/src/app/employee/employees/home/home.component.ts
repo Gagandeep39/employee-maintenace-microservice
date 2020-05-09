4ad508b14da6f0242b30edae1257118d703a7902
Pranav Karmarkar <pranav.karmarkar4@gmail.com>
2020-05-10 04:16:25 +0530

    User Info UI

--- employee-maintenance-frontend/src/app/employee/employees/home/home.component.ts
+++ employee-maintenance-frontend/src/app/employee/employees/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeDetails } from 'src/app/models/employee-details.model';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  employee: EmployeeDetails;
+  constructor() {
+  }
 
   ngOnInit() {
   }
