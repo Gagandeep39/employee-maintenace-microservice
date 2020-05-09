d10e69b8efae330e7043fb3709d0dfe0d1b99730
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-05-09 19:31:17 +0530

    Created a simple footer

--- employee-maintenance-frontend/src/app/shared/footer/footer.component.ts
+++ employee-maintenance-frontend/src/app/shared/footer/footer.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  public now: Date = new Date();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    setInterval(() => {
+      this.now = new Date();
+    }, 1);
   }
 
+  ngOnInit() {}
 }
