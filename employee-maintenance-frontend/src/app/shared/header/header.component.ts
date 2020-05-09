e82e25dc8cad0f5c2448f90b523cb30fa4d44dc5
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-05-09 19:37:04 +0530

    Created header Component Implemented basic logout

--- employee-maintenance-frontend/src/app/shared/header/header.component.ts
+++ employee-maintenance-frontend/src/app/shared/header/header.component.ts
@@ -1,4 +1,12 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-05-09 19:08:31
+ * @modify date 2020-05-09 19:08:31
+ * @desc Header Component Logic
+ */
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +15,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  logOut() : void {
+    this.authService.logOut();
   }
 
 }
