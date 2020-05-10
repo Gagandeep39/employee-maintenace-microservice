79898c28588d75cc14ff93e9ca9dde55051d2706
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-05-10 23:22:53 +0530

    Added update leave url

--- employee-maintenance-frontend/src/environments/environment.ts
+++ employee-maintenance-frontend/src/environments/environment.ts
@@ -13,7 +13,8 @@ export const environment = {
   byId: ':9200/employees/search/id/',
   employeeLeave: ':9300/leaves/employee/',
   leaveBalance: ':9300/leaves/balance/',
-  subEmployeeLeaves: ':9300/leaves/manager/'
+  subEmployeeLeaves: ':9300/leaves/manager/',
+  updateLeave: ':9300/leaves/'
 };
 
 /*
