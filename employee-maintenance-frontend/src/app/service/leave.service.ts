5b79fa435e5228b5ad28a2ae6739c2ffaf32e467
Pranav Karmarkar <pranav.karmarkar4@gmail.com>
2020-05-10 15:13:40 +0530

    Added view leave

--- employee-maintenance-frontend/src/app/service/leave.service.ts
+++ employee-maintenance-frontend/src/app/service/leave.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { LeavePage } from '../models/leave-page.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaveService {
 
-  constructor() { }
+  empId= 100001;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getLeavePages(pageNo: number, pageSize = 10, sortBy = undefined) {
+    let params = new HttpParams();
+    params = params.set('pageNo', pageNo.toString());
+    params = params.set('pageSize', pageSize.toString());
+    if (sortBy !== undefined && sortBy !== null) {
+      params = params.set('sortBy', sortBy.toString());
+    }
+    return this.httpClient.get<LeavePage>(environment.url + environment.employeeLeave + this.empId,
+      {
+        params: params
+      });
+  }
+
+  createLeave()
+  {
+  }
 }
