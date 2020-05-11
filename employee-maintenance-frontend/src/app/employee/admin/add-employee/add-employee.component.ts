a1e5daefc2bc79949dc65e7fb3c419b4082bb932
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-05-11 08:57:42 +0530

    Implemented a dummy emplouyee form

--- employee-maintenance-frontend/src/app/employee/admin/add-employee/add-employee.component.ts
+++ employee-maintenance-frontend/src/app/employee/admin/add-employee/add-employee.component.ts
@@ -1,15 +1,121 @@
 import { Component, OnInit } from '@angular/core';
+import { MaritalStatus } from 'src/app/models/marital-status.model';
+import { Gender } from 'src/app/models/gender.model';
+import { Department } from 'src/app/models/department.model';
+import { Manager } from 'src/app/models/manager.model';
+import { Grade } from 'src/app/models/grade.model';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { User } from 'src/app/models/user.mode';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from 'src/app/service/employee.service';
+import { ValidatorService } from 'src/app/service/validator.service';
+import { CustomValidators } from 'src/app/shared/custom-validators';
 
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
-  styleUrls: ['./add-employee.component.css']
+  styleUrls: ['./add-employee.component.css'],
 })
 export class AddEmployeeComponent implements OnInit {
+  submitted = false;
+  editMode = false;
+  maritalStatus = MaritalStatus;
+  genders = Gender;
+  departments: Department[] = [];
+  managers: Manager[] = [];
+  grades: Grade[] = [];
+  designations : string;
 
-  constructor() { }
+  employeeForm: FormGroup;
+  selectedGrade: Grade;
+
+  isLoading = false;
+  currentUser: User;
+  error = '';
+
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private validatorService: ValidatorService
+  ) {}
 
   ngOnInit() {
+    this.selectedGrade = new Grade();
+    this.employeeForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      dateOfBirth: new FormControl('', [
+        Validators.required,
+        CustomValidators.forbiddenAge,
+      ]),
+      dateOfJoining: new FormControl('', [
+        Validators.required,
+        CustomValidators.forbidFutureDate,
+      ]),
+      maritalStatus: new FormControl('', Validators.required),
+      gender: new FormControl('', Validators.required),
+      departmentId: new FormControl('', Validators.required),
+      managerId: new FormControl('', Validators.required),
+      grade: new FormControl('', Validators.required),
+      designation: new FormControl('', Validators.required),
+      basic: new FormControl('', [
+        Validators.required,
+        (control: FormControl) =>
+          CustomValidators.forbiddenSalary(this.selectedGrade)(control),
+      ]),
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[7-9][0-9]{9}'),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      address: new FormGroup({
+        state: new FormControl('', Validators.required),
+        area: new FormControl('', Validators.required),
+        city: new FormControl('', Validators.required),
+        pincode: new FormControl('', [
+          Validators.required,
+          Validators.pattern('[0-9]{6}'),
+        ]),
+      }),
+    });
+    this.validatorService.fetchAllDepartments().subscribe((response) => (this.departments = response));
+    this.validatorService.fetchAllGrades().subscribe((response) => (this.grades = response));
+    this.validatorService.fetchAllManagers().subscribe((response) => this.managers = response);
   }
 
+  submitForm() {
+    this.submitted = true;
+    if (this.employeeForm.valid) {
+      this.isLoading = true;
+      this.saveDataToServer(this.employeeForm.value);
+    }
+  }
+
+  gradeIsSelected(event) {
+    this.selectedGrade = this.grades.find(
+      (g) => g.gradeCode === event.target.value
+    );
+  }
+
+  saveDataToServer(user: User) {
+    // this.validatorService.addEmployee(this.currentUser, newEmployee).subscribe(
+    //   (response) => {
+    //     console.log(response);
+    //     this.isLoading = false;
+    //     this.error = 'Successfully Created employee with ID: ' + response;
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //     this.isLoading = false;
+    //     this.error = error;
+    //   }
+    // );
+  }
+
+  redirectToHomePage() {
+    // this.router.navigate(['../list'], { relativeTo: this.route });
+  }
+
+  OnInit() {}
 }
