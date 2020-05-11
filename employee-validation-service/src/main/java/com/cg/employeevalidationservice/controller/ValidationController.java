7f6419e92b74f296f036e5cb718c76b2385cffa3
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-05-11 08:34:14 +0530

    Fixed cors error

--- employee-validation-service/src/main/java/com/cg/employeevalidationservice/controller/ValidationController.java
+++ employee-validation-service/src/main/java/com/cg/employeevalidationservice/controller/ValidationController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import com.cg.employeevalidationservice.model.Grade;
 import com.cg.employeevalidationservice.model.Manager;
 import com.cg.employeevalidationservice.service.ValidationService;
 
+@CrossOrigin("*")
 @RestController
 @Slf4j
 public class ValidationController {
