9cb921a7c601ed52ed2ac3a306eee077d2207515
Pranav Karmarkar <pranav.karmarkar4@gmail.com>
2020-05-12 13:26:40 +0530

    Added search by category dropdown

--- employee-maintenance-frontend/src/app/employee/employees/search-employee/search-employee.component.ts
+++ employee-maintenance-frontend/src/app/employee/employees/search-employee/search-employee.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from 'src/app/service/employee.service';
 import { EmployeePage } from 'src/app/models/employee-page.model';
+import { ValidatorService } from 'src/app/service/validator.service';
+import { Department } from 'src/app/models/department.model';
+import { Grade } from 'src/app/models/grade.model';
+import { MaritalStatus } from 'src/app/models/marital-status.model';
 
 @Component({
   selector: 'app-search-employee',
@@ -10,12 +14,18 @@ import { EmployeePage } from 'src/app/models/employee-page.model';
 export class SearchEmployeeComponent implements OnInit {
   public employeePage: EmployeePage = new EmployeePage();
   public currentPage: number;
+  departments : Department[]= [];
+  grades : Grade[]=[];
+  maritalStatuses : MaritalStatus[]=[]
 
-  constructor(private employeeService: EmployeeService) {}
+  constructor(private employeeService: EmployeeService, private validatorService: ValidatorService) {}
 
   ngOnInit() {
     this.fetchDatafromServer(0);
     this.currentPage = 0;
+    this.validatorService.fetchAllDepartments().subscribe(response => this.departments = response);
+    this.validatorService.fetchAllGrades().subscribe(response => this.grades = response);
+    this.maritalStatuses=Object.values(MaritalStatus);
   }
 
   fetchDatafromServer(pageNo: number, pageSize = 10) {
