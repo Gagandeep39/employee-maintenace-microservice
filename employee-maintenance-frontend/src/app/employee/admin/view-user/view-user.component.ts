05d92b5eafbc7a677570b2d040f012487c154ebf
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-05-12 18:22:41 +0530

    Added multi category search in Admin's View employee

--- employee-maintenance-frontend/src/app/employee/admin/view-user/view-user.component.ts
+++ employee-maintenance-frontend/src/app/employee/admin/view-user/view-user.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeePage } from 'src/app/models/employee-page.model';
 import { EmployeeService } from 'src/app/service/employee.service';
+import { Department } from 'src/app/models/department.model';
+import { Grade } from 'src/app/models/grade.model';
+import { MaritalStatus } from 'src/app/models/marital-status.model';
+import { ValidatorService } from 'src/app/service/validator.service';
 
 @Component({
   selector: 'app-view-user',
@@ -11,19 +15,30 @@ export class ViewUserComponent implements OnInit {
 
   public employeePage: EmployeePage = new EmployeePage();
   public currentPage: number;
+  departments : Department[]= [];
+  grades : Grade[]=[];
+  maritalStatuses : MaritalStatus[]=[];
+  category: string = 'all';
+  value: string;
+  nameType : string;
 
-  constructor(private employeeService: EmployeeService) {}
+  constructor(private employeeService: EmployeeService, private validatorService: ValidatorService) {}
 
   ngOnInit() {
     this.fetchDatafromServer(0);
     this.currentPage = 0;
+    this.validatorService.fetchAllDepartments().subscribe(response => this.departments = response);
+    this.validatorService.fetchAllGrades().subscribe(response => this.grades = response);
+    this.maritalStatuses=Object.values(MaritalStatus);
   }
 
   fetchDatafromServer(pageNo: number, pageSize = 10) {
     this.employeeService
-      .getEmployeePages(pageNo, pageSize)
+      .getEmployeeByCategory(pageNo, pageSize, this.category, this.value)
       .subscribe((response) => {
         this.employeePage = response;
+        console.log(response);
+        
       });
   }
 
@@ -44,9 +59,23 @@ export class ViewUserComponent implements OnInit {
     return new Array(i);
   }
 
-  searchByName(searchString: string){
-    this.employeeService.searchByName(searchString).subscribe(response=>{
-      this.employeePage = response;
-    });
+  searchItem(category: string, value: string) {
+    console.log("Executed");
+    
+    this.category = category;
+    this.value = value;
+    this.fetchDatafromServer(0);
+    this.nameType = undefined;
+  }
+
+  searchByName(category: string, value: string) {
+    this.category = category;
+    this.value = value;
+    this.fetchDatafromServer(0);
+    this.nameType = undefined;
+  }
+
+  setSearchType(type: string){
+    this.nameType = type;
   }
 }
