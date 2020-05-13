a6c9d94210ca53e7c0ee9a010f4e7ce5a243ef42
gagandeep39 <singh.gagandeep3911@gmail.com>
2020-05-13 12:42:25 +0530

    Renamed employee-maintenance to employee-management

---
+++ employee-management-service/src/main/java/com/cg/employeemanagementservice/controller/EmployeeRegistrationController.java
@@ -0,0 +1,51 @@
+	package com.cg.employeemanagementservice.controller;
+
+import com.cg.employeemanagementservice.model.EmployeeDetails;
+import com.cg.employeemanagementservice.model.User;
+import com.cg.employeemanagementservice.service.EmployeeRegistrationService;
+import com.cg.employeemanagementservice.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Gagandeep
+ * @date 07-05-2020
+ * @time 01:41
+ */
+
+@CrossOrigin("*")
+@RestController
+public class EmployeeRegistrationController {
+
+    @Autowired
+    private EmployeeRegistrationService employeeRegistrationService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/employees")
+    public ResponseEntity<EmployeeDetails> addEmployee(@RequestBody User user){
+        EmployeeDetails employeeDetails = employeeRegistrationService.addEmployeeDetails(user);
+        return new ResponseEntity<>(employeeDetails, HttpStatus.OK);
+    }
+
+    @PostMapping("/employees/user")
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        User addedUser = userService.addUser(user);
+        return new ResponseEntity<>(addedUser, HttpStatus.OK);
+    }
+
+    @PutMapping("/employees")
+    public ResponseEntity<EmployeeDetails> updateEmployeeDetails(@RequestBody EmployeeDetails details){
+        EmployeeDetails employeeDetails = employeeRegistrationService.updateEmployeeDetails(details);
+        return new ResponseEntity<>(employeeDetails, HttpStatus.OK);
+    }
+
+}
---
+++ employee-management-service/src/main/java/com/cg/employeemanagementservice/service/UserService.java
@@ -0,0 +1,14 @@
+package com.cg.employeemanagementservice.service;
+
+import com.cg.employeemanagementservice.model.User;
+
+/**
+ * @author Gagandeep
+ * @date 07-05-2020
+ * @time 02:13
+ */
+public interface UserService {
+
+    User addUser(User user);
+
+}
