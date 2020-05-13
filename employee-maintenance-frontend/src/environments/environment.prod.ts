7152d4ead8bc4ff1d60d18c2747b852ca8f2ba3b
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-05-13 22:26:24 +0530

    Created a production env Docker only works when angular is built in production

--- employee-maintenance-frontend/src/environments/environment.prod.ts
+++ employee-maintenance-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,26 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'http://192.168.99.100:9900',  // for docker
+  login: '/employee-login-service/login',
+  updateUser: '/employee-login-service/login/update',
+  findUserById: '/employee-login-service/login/',
+  changePassword: '/employee-login-service/login/change',
+
+  addEmployee: '/employee-management-service/employees',
+  fetchAllEmployee: '/employee-management-service/employees/search',
+  searchByCategory: '/employee-management-service/employees/search/category',
+  byName: '/employee-management-service/employees/search/name',
+  byId: '/employee-management-service/employees/search/id/',
+
+  employeeLeave: '/employee-leave-service/leaves/employee/',
+  leaveBalance: '/employee-management-service/leaves/balance/',
+  subEmployeeLeaves: '/employee-management-service/leaves/manager/',
+  updateLeave: '/employee-management-service/leaves/',
+  createLeave: '/employee-management-service/leaves/',
+
+  grades: '/employee-validation-service/grades',
+  departments: '/employee-validation-service/departments',
+  managers: '/employee-validation-service/managers',
+  roles: '/employee-validation-service/roles',
+  validateUsername: '/employee-validation-service/username'
 };
