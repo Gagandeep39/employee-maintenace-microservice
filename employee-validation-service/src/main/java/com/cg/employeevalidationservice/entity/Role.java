9bb38f0e64d42cb0933d19441d64a5d96ad44edf
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-05-14 13:10:05 +0530

    Added Comments i nValidation Entity CLasses

--- employee-validation-service/src/main/java/com/cg/employeevalidationservice/entity/Role.java
+++ employee-validation-service/src/main/java/com/cg/employeevalidationservice/entity/Role.java
@@ -16,6 +16,10 @@ import io.swagger.annotations.ApiModelProperty;
  * @time 20:11
  */
 
+/**
+ * 
+ * Role table to map roles for the user
+ */
 @Entity
 @Table(name = "role_table")
 @Getter
@@ -25,11 +29,17 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel("Role : Stores the roles of user")
 public class Role {
 
+    /**
+     * Role ID
+     */
     @Id
     @Column(name = "role_id")
     @ApiModelProperty("Role ID")
     private Integer roleId;
 
+    /**
+     * Role name
+     */
     @Column(name = "role_name")
     @ApiModelProperty("Role name")
     private String role;
